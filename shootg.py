3ad50c7b256eda8cd10c4f78543aec3074635767
eunjae9988 <eunjae9988@gmail.com>
2026-05-15 02:54:22 +0000

    I made main character dock alien

--- shootg.py
+++ shootg.py
@@ -1,4 +1,9 @@
 import pygame
-import random #random star
-pygame.init() #need for set up
+import random 
+pygame.init() 
 screen=pygame.display.set_mode((500,500))
+def shape_dockalien(surface,x,y):
+    pygame.draw.ellipse(surface,(0,255,0),(x-20,y-15,40,30))
+    pygame.draw.circle(surface,(0,255,0),(x,y-20),10)
+    pygame.draw.circle(surface,(255,0,15),(x-7,y-22),5)
+    pygame.draw.line(surface,(0,255,0),(x-10,y-35),(x+10,y-35),3)
